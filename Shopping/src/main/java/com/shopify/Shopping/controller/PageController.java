a6e580c00ac5f12115476af01a3afdeb7025fd7f
Prity <pritykri@gmail.com>
2019-11-16 17:15:34 +0530

    Url tested

---
+++ Shopping/src/main/java/com/shopify/Shopping/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.shopify.Shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value={"/","/index","/home"})
+	public ModelAndView index() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Shoppimg");
+		return mv;
+	}
+}
