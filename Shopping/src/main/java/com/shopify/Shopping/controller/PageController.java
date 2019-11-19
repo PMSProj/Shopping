0d7d10e90efd75b22fdfebc5b515f31b644e5929
Prity <pritykri@gmail.com>
2019-11-19 17:06:54 +0530

    Completed interface

--- Shopping/src/main/java/com/shopify/Shopping/controller/PageController.java
+++ Shopping/src/main/java/com/shopify/Shopping/controller/PageController.java
@@ -7,9 +7,26 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 	@RequestMapping(value={"/","/index","/home"})
-	public ModelAndView index() {
+	public ModelAndView index() { 
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Shoppimg");
+		mv.addObject("title","Home");
+		mv.addObject("userClickedHome",true);
+		return mv;
+	}
+	
+	@RequestMapping(value="/about")
+	public ModelAndView about() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickedAbout",true);
+		return mv;
+	}
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Contact");
+		mv.addObject("userClickedContact",true);
 		return mv;
 	}
 }
---
+++ Shopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class='row'>
+		<hr />
+		<p1>This is contact us page</p1>
+
+	</div>
+</div>
